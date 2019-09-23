4b0d7d9284cc57b844d1f454ce2ba1a76c920ead
Euan <euan2@mac.com>
2019-09-23 07:55:44 +0100

    Initial Commit

---
+++ PocketMoneyTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PocketMoneyTracker
+//
+//  Created by Euan Macfarlane on 23/09/2019.
+//  Copyright © 2019 Euan Macfarlane. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
